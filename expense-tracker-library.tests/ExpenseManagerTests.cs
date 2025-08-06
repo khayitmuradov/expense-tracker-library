9a672ce201e0cc3c78881c73710d8eb0c09b3890
khayitmuradov <khayitmuradov@proton.me>
2025-08-06 16:15:17 -0500

    style: deleted useless comments in ExpenseManagerTests.cs

--- expense-tracker-library.tests/ExpenseManagerTests.cs
+++ expense-tracker-library.tests/ExpenseManagerTests.cs
@@ -11,7 +11,6 @@ namespace expense_tracker_library.tests
         [InlineData(35, "Dinner", "2023-10-01T00:00:00Z", "Dinner with Family")]
         public void AddExpense_ShouldWork(int amount, string category, DateTime dateString, string description)
         {
-            //arrange
             Expense newExpense = new Expense
             {
                 Amount = amount,
@@ -21,11 +20,9 @@ namespace expense_tracker_library.tests
             };
             ExpenseManager expenseManager = new ExpenseManager();
 
-            //act
             expenseManager.AddExpense(newExpense);
             var allExpenses = expenseManager.GetAllExpenses();
 
-            //assert
             Assert.True(allExpenses.Count == 1);
             Assert.Contains<Expense>(newExpense, allExpenses);
         }
@@ -33,18 +30,14 @@ namespace expense_tracker_library.tests
         [Fact]
         public void AddExpense_ShouldThrow_WhenExpenseIsNull()
         {
-            //arrange
             ExpenseManager expenseManager = new ExpenseManager();
 
-
-            //act n assert
             Assert.Throws<ArgumentNullException>(() => expenseManager.AddExpense(null!));
         }
 
         [Fact]
         public void GetTotalExpenses_ShouldReturnCorrectSum()
         {
-            //arrange
             ExpenseManager expenseManager = new ExpenseManager();
             Expense expense1 = new Expense()
             {
@@ -68,20 +61,17 @@ namespace expense_tracker_library.tests
                 Description = "Morning Coffee"
             };
 
-            //act
             expenseManager.AddExpense(expense1);
             expenseManager.AddExpense(expense2);
             expenseManager.AddExpense(expense3);
             var actualSumOfExpenses = expenseManager.GetTotalExpenses();
 
-            //assert
             Assert.Equal(40.75m, actualSumOfExpenses);
         }
 
         [Fact]
         public void GetTotalByCategory_ShouldReturnCorrectSum()
         {
-            //arrange
             Expense expense1 = new Expense()
             {
                 Amount = 23m,
@@ -112,14 +102,12 @@ namespace expense_tracker_library.tests
             };
             ExpenseManager expenseManager = new ExpenseManager();
 
-            //act
             expenseManager.AddExpense(expense1);
             expenseManager.AddExpense(expense2);
             expenseManager.AddExpense(expense3);
             expenseManager.AddExpense(expense4);
             var actualTotalByCategory = expenseManager.GetTotalByCategory(expense1.Category);
 
-            //assert
             Assert.Equal(35.46m, actualTotalByCategory);
         }
 
@@ -128,7 +116,6 @@ namespace expense_tracker_library.tests
         [InlineData("Entertainment")]
         public void GetTotalByCategory_ShouldBeZero_IfCategoryNotFound(string category)
         {
-            //arrange
             Expense newExpense = new Expense()
             {
                 Amount = 45m,
@@ -136,18 +123,15 @@ namespace expense_tracker_library.tests
             };
             ExpenseManager expenseManager = new ExpenseManager();
 
-            //act
             expenseManager.AddExpense(newExpense);
             var actualResult = expenseManager.GetTotalByCategory("Bills");
 
-            //assert
             Assert.Equal(0m, actualResult);
         }
 
         [Fact]
         public void GetGetExpensesByDateRange_ShouldReturnCorrectExpense()
         {
-            //arrange
             var expense1 = new Expense
             {
                 Amount = 10,
@@ -177,13 +161,11 @@ namespace expense_tracker_library.tests
             DateTime startDate = new DateTime(2025, 08, 01);
             DateTime endDate = new DateTime(2025, 08, 02);
 
-            //act
             expenseManager.AddExpense(expense1);
             expenseManager.AddExpense(expense2);
             expenseManager.AddExpense(expense3);
             var actualResult = expenseManager.GetExpensesByDateRange(startDate, endDate);
 
-            //assert
             Assert.Single(actualResult);
             Assert.Contains(expense1, actualResult);
         }
@@ -202,11 +184,9 @@ namespace expense_tracker_library.tests
             DateTime startDate = new DateTime(2025, 08, 10);
             DateTime endDate = new DateTime(2025, 08, 15);
 
-            //act
             expenseManager.AddExpense(expense);
             var actualResult = expenseManager.GetExpensesByDateRange(startDate, endDate);
 
-            //assert
             Assert.Empty(actualResult);
         }
 
@@ -236,14 +216,12 @@ namespace expense_tracker_library.tests
             };
             ExpenseManager expenseManager = new ExpenseManager();
             
-            //act
             expenseManager.AddExpense(expense1);
             expenseManager.AddExpense(expense2);
             expenseManager.AddExpense(expense3);
 
             var actualResult = expenseManager.GetAllExpenses();
 
-            //assert
             Assert.Equal(3, actualResult.Count);
             Assert.Contains(expense1, actualResult);
             Assert.Contains(expense3, actualResult);
